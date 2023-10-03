13f919f37eff7153cf62dac2a509f57a83191393
sonu roy <sonuroy2093@gmail.com>
2023-10-03 20:43:52 +0530

    i have add 1 more condition

--- JavaBasicProgram/src/Com/Nit/JavaProgram/AddTwoString.java
+++ JavaBasicProgram/src/Com/Nit/JavaProgram/AddTwoString.java
@@ -7,11 +7,17 @@ public class AddTwoString {
 		
 		String str1 = "Sonu";
         String str2 = "Roy";
+        
+        String str3 = "archana";
+        String str4 = "Roy";
 
         // Using the + operator
         String result1 = str1 + " " + str2;
         System.out.println("Result using + operator: " + result1);
 
+        String result3 = str3 + " " + str4;
+        System.out.println("Result using + operator: " + result3);
+        
         // Using the concat() method
         String result2 = str1.concat(" ").concat(str2);
         System.out.println("Result using concat() method: " + result2);
